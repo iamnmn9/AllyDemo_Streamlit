b3b790c865a7f8185dc4711ddc0496fb7e3f98a6
4e414d414e <41872440+iamnmn9@users.noreply.github.com>
2023-10-27 18:51:33 -0400

    Update chat.py

--- chat.py
+++ chat.py
@@ -71,7 +71,7 @@ st.markdown("<div class='bot-message'>Ally: Hey, I am Ally :) What's your name?<
 user_input = st.text_input("You: ")
 
 if user_input:
-    st.markdown(f"<div class='bot-message'>Ally: Hi {user_input}!</div>", unsafe_allow_html=True)
+    st.markdown(f"<div class='bot-message'>Ally: Hey {user_input} :) </div>", unsafe_allow_html=True)
     st.markdown(f"<div class='bot-message'>Ally: I'm here to help! Feel free to ask me anything.</div>", unsafe_allow_html=True)
 
 st.markdown("</div>", unsafe_allow_html=True)
