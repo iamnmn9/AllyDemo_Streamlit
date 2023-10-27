0845e724215eddb0fe76ce1bd1bb98f8158ac49b
4e414d414e <41872440+iamnmn9@users.noreply.github.com>
2023-10-27 18:11:09 -0400

    Update chat.py

--- chat.py
+++ chat.py
@@ -5,7 +5,7 @@ from PIL import Image
 st.set_page_config(
     page_title="ALLY COPILOT DEMO",
     page_icon="🦋",
-    layout="wide",  # Wide layout for the entire page
+    layout="centered",  # Center the content
 )
 
 # Custom CSS for Chatbox Style
@@ -13,64 +13,39 @@ st.markdown(
     """
     <style>
     body {
-        background-color: #f0f5f9; /* Light blue background */
-        font-family: 'Arial', sans-serif;
+        background-color: #f2f2f2; /* Light grey background */
     }
-    .main-container {
+    .chat-container {
         display: flex;
         flex-direction: column;
         align-items: center;
-        justify-content: center;
-        height: 100vh; /* 100% of the viewport height */
     }
-    .chatbox-container {
-        border: 2px solid #4caf50; /* Green border for chatbox */
-        border-radius: 20px;
-        padding: 30px;
-        width: 80%;
+    .chatbox {
+        border: 2px solid #8dcfe3; /* Grey border for chatbox */
+        border-radius: 12px;
+        padding: 20px;
+        margin-top: 20px;
         max-width: 500px;
+        width: 100%;
         background-color: #ffffff; /* White background for chatbox */
-        box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
     }
     .user-message {
-        background-color: #4caf50; /* Green background for user messages */
-        color: white;
-        padding: 10px 20px;
-        border-radius: 25px;
+        background-color: #e6a6ed; /* Grey background for user messages */
+        padding: 10px;
+        border-radius: 10px;
         margin-bottom: 10px;
         text-align: left;
-        max-width: 70%;
-        word-wrap: break-word;
     }
     .bot-message {
-        background-color: #ff9800; /* Orange background for Ally's messages */
-        padding: 10px 20px;
-        border-radius: 25px;
+        background-color: #ffd1dc; /* Light pink background for Ally's messages */
+        padding: 10px;
+        border-radius: 10px;
         margin-bottom: 10px;
         text-align: left;
-        max-width: 70%;
-        word-wrap: break-word;
     }
-    .input-field {
-        width: 100%;
-        padding: 15px;
-        border: 2px solid #4caf50; /* Green border for input field */
-        border-radius: 25px;
-        font-size: 16px;
-        margin-bottom: 20px;
-    }
-    .submit-button {
-        background-color: #4caf50; /* Green button background */
-        color: white;
-        padding: 15px 30px;
-        border: none;
-        border-radius: 25px;
-        font-size: 18px;
-        cursor: pointer;
-        transition: background-color 0.3s ease;
-    }
-    .submit-button:hover {
-        background-color: #45a049; /* Darker green on hover */
+    .logo {
+        max-width: 100%; /* Make the logo responsive */
+        margin-bottom: 10px;
     }
     </style>
     """,
@@ -80,18 +55,21 @@ st.markdown(
 # Ally Logo
 ally_logo_path = "ally.png"
 ally_logo = Image.open(ally_logo_path)
+st.markdown("<style>.logo { max-width: 150px; margin-bottom: 10px; }</style>", unsafe_allow_html=True)
 st.image(ally_logo, use_column_width=False, width=150)
 
-# Chatbox Container
-st.markdown("<div class='main-container'>", unsafe_allow_html=True)
-st.markdown("<div class='chatbox-container'>", unsafe_allow_html=True)
+# Chatbox
+st.markdown("<div class='chat-container'>", unsafe_allow_html=True)
+st.header("ALLY COPILOT DEMO")
+st.markdown("<div class='chatbox'>", unsafe_allow_html=True)
 
 # Chat Conversation
-user_input = st.text_input("You: ", key="input-field")
+user_input = st.text_input("You: ")
 
-if st.button("Send", key="submit-button"):
-    st.markdown("<div class='user-message'>You: {}</div>".format(user_input), unsafe_allow_html=True)
-    st.markdown("<div class='bot-message'>Ally: Hi there! How can I assist you today?</div>", unsafe_allow_html=True)
+if user_input:
+    st.markdown("<div class='bot-message'>Ally: Hi I am Ally! What's your name?.</div>", unsafe_allow_html=True)
+    st.markdown(f"<div class='user-message'>You: {user_input}</div>", unsafe_allow_html=True)
+    st.markdown("<div class='bot-message'>Ally: I'm here to help! Feel free to ask me anything.</div>", unsafe_allow_html=True)
 
 st.markdown("</div>", unsafe_allow_html=True)
 st.markdown("</div>", unsafe_allow_html=True)
