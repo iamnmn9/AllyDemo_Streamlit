6f136acb80630f55a6250ac63b4897caa717ea41
4e414d414e <41872440+iamnmn9@users.noreply.github.com>
2023-10-27 17:16:20 -0400

    Update chat.py

--- chat.py
+++ chat.py
@@ -8,59 +8,66 @@ st.set_page_config(
     layout="centered",  # Center the content
 )
 
-# Custom CSS for Chatbot Style
-st.markdown(
-    """
-    <style>
-    .chat-container {
-        display: flex;
-        flex-direction: column;
-        align-items: flex-start;
-    }
-    .user-message {
-        background-color: #e6f7ff;
-        padding: 10px;
-        border-radius: 10px;
-        margin: 10px;
-        max-width: 70%;
-        text-align: left;
-    }
-    .bot-message {
-        background-color: #b3e0ff;
-        padding: 10px;
-        border-radius: 10px;
-        margin: 10px;
-        max-width: 70%;
-        text-align: left;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
-
-# Set Background Color to White
+# Custom CSS for Chatbox Style
 st.markdown(
     """
     <style>
     body {
-        background-color: #ffffff;
+        background-color: #f2f2f2; /* Light grey background */
+    }
+    .chat-container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    .chatbox {
+        border: 1px solid #cccccc; /* Grey border for chatbox */
+        border-radius: 10px;
+        padding: 20px;
+        margin-top: 20px;
+        max-width: 500px;
+        width: 100%;
+        background-color: #ffffff; /* White background for chatbox */
+    }
+    .user-message {
+        background-color: #f2f2f2; /* Grey background for user messages */
+        padding: 10px;
+        border-radius: 10px;
+        margin-bottom: 10px;
+        text-align: left;
+    }
+    .bot-message {
+        background-color: #ffd1dc; /* Light pink background for Ally's messages */
+        padding: 10px;
+        border-radius: 10px;
+        margin-bottom: 10px;
+        text-align: left;
+    }
+    .logo {
+        max-width: 150px;
+        margin-bottom: 20px;
     }
     </style>
     """,
     unsafe_allow_html=True,
 )
 
-# Ally Image
-ally_image_path = "ally.png"
-ally_image = Image.open(ally_image_path)
-st.image(ally_image, use_column_width=True)
+# Ally Logo
+ally_logo_path = "ally.png"
+ally_logo = Image.open(ally_logo_path)
+st.image(ally_logo, caption="Ally Logo", use_column_width=False, width=150, class="logo")
 
-# Chatbot Interaction
+# Chatbox
 st.markdown("<div class='chat-container'>", unsafe_allow_html=True)
-st.markdown("<div class='bot-message'>ALLY: Hello there! I'm Ally, your virtual assistant. How can I assist you today?</div>", unsafe_allow_html=True)
+st.header("ALLY COPILOT DEMO")
+st.markdown("<div class='chatbox'>", unsafe_allow_html=True)
+
+# Chat Conversation
 user_input = st.text_input("You: ")
 
 if user_input:
     st.markdown(f"<div class='user-message'>YOU: {user_input}</div>", unsafe_allow_html=True)
     st.markdown("<div class='bot-message'>ALLY: I'm here to help! Feel free to ask me anything.</div>", unsafe_allow_html=True)
+
+st.markdown("</div>", unsafe_allow_html=True)
 st.markdown("</div>", unsafe_allow_html=True)
