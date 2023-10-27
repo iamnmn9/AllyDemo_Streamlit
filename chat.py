3915769e85d5557614ff381a68229a7d2646d9a8
4e414d414e <41872440+iamnmn9@users.noreply.github.com>
2023-10-27 19:01:56 -0400

    Update chat.py

--- chat.py
+++ chat.py
@@ -13,26 +13,24 @@ st.markdown(
     """
     <style>
     body {
-        background-color: #333333; /* Dark grey background */
-        color: white; /* White text color */
-        font-family: Arial, sans-serif; /* Font style */
+        background-color: #333333; /* Light black background */
     }
     .chat-container {
         display: flex;
         flex-direction: column;
         align-items: center;
         justify-content: center;
-        margin-top: 20px;
     }
     .chatbox {
-        border: 2px solid #8dcfe3; /* Light blue border for chatbox */
+        border: 2px solid #8dcfe3; /* Grey border for chatbox */
         border-radius: 12px;
         padding: 20px;
+        margin-top: 20px;
         max-width: 500px;
         width: 100%;
     }
     .user-message {
-        background-color:  #b9e2eb; /* Light blue background for user messages */
+        background-color:  #b9e2eb; /* Grey background for user messages */
         padding: 10px;
         border-radius: 10px;
         margin-bottom: 10px;
@@ -45,36 +43,45 @@ st.markdown(
         margin-bottom: 10px;
         text-align: left;
     }
-    .logo {
+     .logo {
         max-width: 100%; /* Make the logo responsive */
-        margin-bottom: 20px;
+        margin-bottom: 1px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
     }
     .header {
         text-align: center;
         font-size: 36px;
         margin-bottom: 20px;
-        color: #8dcfe3; /* Light blue color for header */
+        color: #333333; /* Light blue color for header */
     }
     </style>
     """,
     unsafe_allow_html=True,
 )
-
-# Chatbox
-st.markdown("<div class='chat-container'>", unsafe_allow_html=True)
+st.markdown("<div class='header'>Ally: Your Copilot!</div>", unsafe_allow_html=True)
+# Ally Logo
 st.markdown("<div class='logo'>")
 ally_logo_path = "ally.png"
 ally_logo = Image.open(ally_logo_path)
 st.image(ally_logo, use_column_width=False, width=300)
 st.markdown("</div>")
 
-st.markdown("<div class='header'>Ally: Your Copilot!</div>", unsafe_allow_html=True)
+
+
+# Chatbox
+st.markdown("<div class='chat-container'>", unsafe_allow_html=True)
+# st.header("Ally: Your Copilot!")
+
+st.markdown("<div class='bot-message'>Ally: Hey, I am Ally :) What's your name?</div>", unsafe_allow_html=True)
 
 # Chat Conversation
 user_input = st.text_input("You: ")
 
 if user_input:
-    st.markdown(f"<div class='user-message'>You: {user_input}</div>", unsafe_allow_html=True)
-    st.markdown("<div class='bot-message'>Ally: Hey {user_input}! I'm here to help! Feel free to ask me anything.</div>", unsafe_allow_html=True)
+    st.markdown(f"<div class='bot-message'>Ally: Hey {user_input} :) </div>", unsafe_allow_html=True)
+    st.markdown(f"<div class='bot-message'>Ally: I'm here to help! Feel free to ask me anything.</div>", unsafe_allow_html=True)
 
+st.markdown("</div>", unsafe_allow_html=True)
 st.markdown("</div>", unsafe_allow_html=True)
