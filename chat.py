b3a71d3c6faac069cc6be4a3101be226aefb6281
4e414d414e <41872440+iamnmn9@users.noreply.github.com>
2023-10-27 18:01:32 -0400

    Update chat.py

--- chat.py
+++ chat.py
@@ -5,7 +5,7 @@ from PIL import Image
 st.set_page_config(
     page_title="ALLY COPILOT DEMO",
     page_icon="🦋",
-    layout="centered",  # Center the content
+    layout="wide",  # Wide layout for the entire page
 )
 
 # Custom CSS for Chatbox Style
@@ -14,37 +14,22 @@ st.markdown(
     <style>
     body {
         background-color: #f2f2f2; /* Light grey background */
-    }
-    .chat-container {
         display: flex;
         flex-direction: column;
         align-items: center;
+        justify-content: center;
+        height: 100vh; /* 100% of the viewport height */
     }
     .chatbox {
         border: 2px solid #8dcfe3; /* Grey border for chatbox */
         border-radius: 12px;
         padding: 20px;
-        margin-top: 20px;
+        width: 80%;
         max-width: 500px;
-        width: 100%;
         background-color: #ffffff; /* White background for chatbox */
     }
-    .user-message {
-        background-color: #e6a6ed; /* Grey background for user messages */
-        padding: 10px;
-        border-radius: 10px;
-        margin-bottom: 10px;
-        text-align: left;
-    }
-    .bot-message {
-        background-color: #ffd1dc; /* Light pink background for Ally's messages */
-        padding: 10px;
-        border-radius: 10px;
-        margin-bottom: 10px;
-        text-align: left;
-    }
     .logo {
-        max-width: 100%; /* Make the logo responsive */
+        max-width: 150px;
         margin-bottom: 10px;
     }
     </style>
@@ -55,12 +40,9 @@ st.markdown(
 # Ally Logo
 ally_logo_path = "ally.png"
 ally_logo = Image.open(ally_logo_path)
-st.markdown("<style>.logo { max-width: 150px; margin-bottom: 10px; }</style>", unsafe_allow_html=True)
-st.image(ally_logo, use_column_width=False, width=150)
+st.image(ally_logo, caption="Ally Logo", use_column_width=False, width=150, class="logo")
 
 # Chatbox
-st.markdown("<div class='chat-container'>", unsafe_allow_html=True)
-st.header("ALLY COPILOT DEMO")
 st.markdown("<div class='chatbox'>", unsafe_allow_html=True)
 
 # Chat Conversation
@@ -70,5 +52,4 @@ user_company = st.text_input("Your Company:")
 if user_name and user_company:
     st.markdown("<div class='bot-message'>Ally: Hi, {} from {}! How can I assist you today?</div>".format(user_name, user_company), unsafe_allow_html=True)
 
-st.markdown("</div>", unsafe_allow_html=True)
 st.markdown("</div>", unsafe_allow_html=True)
