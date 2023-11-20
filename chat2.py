6ec2a970a79ea0e1ec623925ea93db6d17119b9e
4e414d414e <41872440+iamnmn9@users.noreply.github.com>
2023-11-20 14:46:23 -0500

    Update chat2.py

--- chat2.py
+++ chat2.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import openai
 from PIL import Image
 
 # Page Configuration
@@ -8,13 +9,13 @@ st.set_page_config(
     layout="wide",
 )
 
-# Custom CSS for Facebook-like Theme
+# Custom CSS for Enhanced Design
 st.markdown(
     """
     <style>
     body {
         font-family: 'Arial', sans-serif;
-        background-color: #f0f2f5; /* Light grayish-blue background similar to Facebook */
+        background-color: #f0f2f5;
         margin: 0;
         padding: 0;
     }
@@ -22,7 +23,7 @@ st.markdown(
         text-align: center;
         font-size: 48px;
         margin-bottom: 20px;
-        color: #1877f2; /* Facebook blue color for the header */
+        color: #1877f2;
     }
     .chat-container {
         display: flex;
@@ -32,38 +33,37 @@ st.markdown(
         padding: 20px;
     }
     .chatbox {
-        border: 1px solid #dddfe2; /* Light border color */
+        border: 1px solid #dddfe2;
         border-radius: 12px;
         padding: 20px;
         margin-top: 20px;
         max-width: 800px;
         width: 100%;
-        background-color: #ffffff; /* White chatbox background */
+        background-color: #ffffff;
+    }
+    .message {
+        padding: 10px;
+        border-radius: 10px;
+        margin-bottom: 10px;
+        text-align: left;
+        width: 80%;
+        max-width: 70%;
     }
     .user-message {
-        background-color: #e9f0f4; /* Light blue background for user messages */
-        color: #1c1e21; /* Dark text color */
-        padding: 10px;
-        border-radius: 10px;
-        margin-bottom: 10px;
-        text-align: left;
-        width: 70%;
+        background-color: #1877f2;
+        color: white;
+        align-self: flex-end;
     }
     .bot-message {
-        background-color: #ffffff; /* White background for Ally's messages */
-        color: #1c1e21; /* Dark text color */
-        padding: 10px;
-        border-radius: 10px;
-        margin-bottom: 10px;
-        text-align: left;
-        width: 70%;
+        background-color: #f3f3f3;
+        color: #1c1e21;
     }
     .input-box {
         width: 70%;
         margin-bottom: 20px;
     }
     .button-style {
-        background-color: #1877f2; /* Facebook blue button color */
+        background-color: #1877f2;
         color: white;
         border: none;
         padding: 10px 20px;
@@ -72,11 +72,11 @@ st.markdown(
         transition: background-color 0.3s ease-in-out;
     }
     .button-style:hover {
-        background-color: #0e5a8a; /* Darker blue on button hover */
+        background-color: #0e5a8a;
     }
     .logo-top-left {
-        width: 100px; /* Adjust the width of the logo */
-        height: auto; /* Maintain aspect ratio */
+        width: 100px;
+        height: auto;
         margin-bottom: 20px;
     }
     </style>
@@ -84,6 +84,7 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
+# Header
 st.markdown("<div class='header'>ALLY : YOUR COPILOT (DEMO)</div>", unsafe_allow_html=True)
 
 # Logo at the top left
@@ -93,33 +94,24 @@ st.image(logo_top_left, use_column_width=False, caption="Ally Logo")
 
 # Chatbox
 st.markdown("<div class='chat-container'>", unsafe_allow_html=True)
-st.markdown("<div class='bot-message'>Ally: Hey, I am Ally :) What's your name?</div>", unsafe_allow_html=True)
+expander = st.beta_expander("Ally: Hey, I am Ally :) What's your name?")
+with expander:
+    initial_user_name = st.text_input("You:")
 
-initial_user_name = st.text_input("You: ")
+    if initial_user_name:
+        st.markdown(f"Ally: Hey {initial_user_name} :)")
+        st.markdown("Ally: To get started, I'd like to learn more about your business.")
 
-if initial_user_name:
-    st.markdown(f"<div class='bot-message'>Ally: Hey {initial_user_name} :) </div>", unsafe_allow_html=True)
-    st.markdown("<div class='bot-message'>Ally: To get started, I'd like to learn more about your business.</div>", unsafe_allow_html=True)
+        # Expanding sections for business details
+        with st.beta_expander("Tell me about your business"):
+            user_business_name = st.text_input("Your Business Name:")
+            user_business_email = st.text_input("Business Email:")
+            user_business_description = st.text_area(f"{user_business_name}'s Description:")
+            user_primary_goal = st.text_input(f"{initial_user_name}'s Primary Goal:")
 
-    # Add your initial questions for the user here
-    user_business_name = st.text_input("Your Business Name: ")
-    user_business_email = st.text_input("Business Email: ")
-    
-    if user_business_name and user_business_email:
-        st.markdown(f"<div class='bot-message'>Ally: Great! Tell me more about {user_business_name}. What do you do?</div>", unsafe_allow_html=True)
-        user_business_description = st.text_input(f"{user_business_name}'s Description: ")
-        
-        if user_business_description:
-            st.markdown(f"<div class='bot-message'>Ally: Thanks for sharing. What's your primary goal, {initial_user_name}?</div>", unsafe_allow_html=True)
-            user_primary_goal = st.text_input(f"{initial_user_name}'s Primary Goal: ")
-
-            if user_primary_goal:
-                st.markdown(f"<div class='bot-message'>Ally: Got it! Feel free to ask any questions or share your thoughts.</div>", unsafe_allow_html=True)
-                
-                # User input and OpenAI response
-                user_input1 = st.text_input("You:", key="input1")
-                # Set your OpenAI API key
-                openai_api_key = st.secrets["openai_api_key"]["key"]
+            # OpenAI response
+            with st.beta_expander("Ask me anything or share your thoughts"):
+                user_input1 = st.text_input("You:")
                 if user_input1:
                     completions = openai.Completion.create(
                         engine="text-davinci-003",
@@ -130,10 +122,7 @@ if initial_user_name:
                         temperature=0.5,
                     )
                     output = completions.choices[0].text.lstrip("\n")
-                
-                    # Store the output
-                    st.markdown(f"<div class='user-message'>You: {user_input1}</div>", unsafe_allow_html=True)
-                    st.markdown(f"<div class='bot-message'>Ally: {output}</div>", unsafe_allow_html=True)
+                    st.markdown(f"**You:** {user_input1}", unsafe_allow_html=True)
+                    st.markdown(f"**Ally:** {output}", unsafe_allow_html=True)
 
-st.markdown("</div>", unsafe_allow_html=True)
 st.markdown("</div>", unsafe_allow_html=True)
