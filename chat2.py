d95f8689804a11142ffa01ba4b5bd5c035dd1326
4e414d414e <41872440+iamnmn9@users.noreply.github.com>
2023-11-20 14:49:10 -0500

    Update chat2.py

--- chat2.py
+++ chat2.py
@@ -1,128 +1,110 @@
-import streamlit as st
 import openai
-from PIL import Image
+import streamlit as st
+from streamlit_chat import message
 
-# Page Configuration
-st.set_page_config(
-    page_title="Ally Demo",
-    page_icon="🦋",
-    layout="wide",
-)
+# Setting page title and header
+st.set_page_config(page_title="AVA", page_icon=":robot_face:")
+st.markdown("<h1 style='text-align: center;'>AVA - a totally harmless chatbot 😬</h1>", unsafe_allow_html=True)
 
-# Custom CSS for Enhanced Design
-st.markdown(
-    """
-    <style>
-    body {
-        font-family: 'Arial', sans-serif;
-        background-color: #f0f2f5;
-        margin: 0;
-        padding: 0;
-    }
-    .header {
-        text-align: center;
-        font-size: 48px;
-        margin-bottom: 20px;
-        color: #1877f2;
-    }
-    .chat-container {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        padding: 20px;
-    }
-    .chatbox {
-        border: 1px solid #dddfe2;
-        border-radius: 12px;
-        padding: 20px;
-        margin-top: 20px;
-        max-width: 800px;
-        width: 100%;
-        background-color: #ffffff;
-    }
-    .message {
-        padding: 10px;
-        border-radius: 10px;
-        margin-bottom: 10px;
-        text-align: left;
-        width: 80%;
-        max-width: 70%;
-    }
-    .user-message {
-        background-color: #1877f2;
-        color: white;
-        align-self: flex-end;
-    }
-    .bot-message {
-        background-color: #f3f3f3;
-        color: #1c1e21;
-    }
-    .input-box {
-        width: 70%;
-        margin-bottom: 20px;
-    }
-    .button-style {
-        background-color: #1877f2;
-        color: white;
-        border: none;
-        padding: 10px 20px;
-        border-radius: 5px;
-        cursor: pointer;
-        transition: background-color 0.3s ease-in-out;
-    }
-    .button-style:hover {
-        background-color: #0e5a8a;
-    }
-    .logo-top-left {
-        width: 100px;
-        height: auto;
-        margin-bottom: 20px;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
+# Set org ID and API key
+openai.organization = "<YOUR_OPENAI_ORG_ID>"
+openai.api_key = "<YOUR_OPENAI_API_KEY>"
 
-# Header
-st.markdown("<div class='header'>ALLY : YOUR COPILOT (DEMO)</div>", unsafe_allow_html=True)
+# Initialise session state variables
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+if 'messages' not in st.session_state:
+    st.session_state['messages'] = [
+        {"role": "system", "content": "You are a helpful assistant."}
+    ]
+if 'model_name' not in st.session_state:
+    st.session_state['model_name'] = []
+if 'cost' not in st.session_state:
+    st.session_state['cost'] = []
+if 'total_tokens' not in st.session_state:
+    st.session_state['total_tokens'] = []
+if 'total_cost' not in st.session_state:
+    st.session_state['total_cost'] = 0.0
 
-# Logo at the top left
-logo_top_left_path = "ally.png"
-logo_top_left = Image.open(logo_top_left_path)
-st.image(logo_top_left, use_column_width=False, caption="Ally Logo")
+# Sidebar - let user choose model, show total cost of current conversation, and let user clear the current conversation
+st.sidebar.title("Sidebar")
+model_name = st.sidebar.radio("Choose a model:", ("GPT-3.5", "GPT-4"))
+counter_placeholder = st.sidebar.empty()
+counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
+clear_button = st.sidebar.button("Clear Conversation", key="clear")
 
-# Chatbox
-st.markdown("<div class='chat-container'>", unsafe_allow_html=True)
-expander = st.beta_expander("Ally: Hey, I am Ally :) What's your name?")
-with expander:
-    initial_user_name = st.text_input("You:")
+# Map model names to OpenAI model IDs
+if model_name == "GPT-3.5":
+    model = "gpt-3.5-turbo"
+else:
+    model = "gpt-4"
 
-    if initial_user_name:
-        st.markdown(f"Ally: Hey {initial_user_name} :)")
-        st.markdown("Ally: To get started, I'd like to learn more about your business.")
+# reset everything
+if clear_button:
+    st.session_state['generated'] = []
+    st.session_state['past'] = []
+    st.session_state['messages'] = [
+        {"role": "system", "content": "You are a helpful assistant."}
+    ]
+    st.session_state['number_tokens'] = []
+    st.session_state['model_name'] = []
+    st.session_state['cost'] = []
+    st.session_state['total_cost'] = 0.0
+    st.session_state['total_tokens'] = []
+    counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
 
-        # Expanding sections for business details
-        with st.beta_expander("Tell me about your business"):
-            user_business_name = st.text_input("Your Business Name:")
-            user_business_email = st.text_input("Business Email:")
-            user_business_description = st.text_area(f"{user_business_name}'s Description:")
-            user_primary_goal = st.text_input(f"{initial_user_name}'s Primary Goal:")
 
-            # OpenAI response
-            with st.beta_expander("Ask me anything or share your thoughts"):
-                user_input1 = st.text_input("You:")
-                if user_input1:
-                    completions = openai.Completion.create(
-                        engine="text-davinci-003",
-                        prompt=user_input1,
-                        max_tokens=1024,
-                        n=1,
-                        stop=None,
-                        temperature=0.5,
-                    )
-                    output = completions.choices[0].text.lstrip("\n")
-                    st.markdown(f"**You:** {user_input1}", unsafe_allow_html=True)
-                    st.markdown(f"**Ally:** {output}", unsafe_allow_html=True)
+# generate a response
+def generate_response(prompt):
+    st.session_state['messages'].append({"role": "user", "content": prompt})
 
-st.markdown("</div>", unsafe_allow_html=True)
+    completion = openai.ChatCompletion.create(
+        model=model,
+        messages=st.session_state['messages']
+    )
+    response = completion.choices[0].message.content
+    st.session_state['messages'].append({"role": "assistant", "content": response})
+
+    # print(st.session_state['messages'])
+    total_tokens = completion.usage.total_tokens
+    prompt_tokens = completion.usage.prompt_tokens
+    completion_tokens = completion.usage.completion_tokens
+    return response, total_tokens, prompt_tokens, completion_tokens
+
+
+# container for chat history
+response_container = st.container()
+# container for text box
+container = st.container()
+
+with container:
+    with st.form(key='my_form', clear_on_submit=True):
+        user_input = st.text_area("You:", key='input', height=100)
+        submit_button = st.form_submit_button(label='Send')
+
+    if submit_button and user_input:
+        output, total_tokens, prompt_tokens, completion_tokens = generate_response(user_input)
+        st.session_state['past'].append(user_input)
+        st.session_state['generated'].append(output)
+        st.session_state['model_name'].append(model_name)
+        st.session_state['total_tokens'].append(total_tokens)
+
+        # from https://openai.com/pricing#language-models
+        if model_name == "GPT-3.5":
+            cost = total_tokens * 0.002 / 1000
+        else:
+            cost = (prompt_tokens * 0.03 + completion_tokens * 0.06) / 1000
+
+        st.session_state['cost'].append(cost)
+        st.session_state['total_cost'] += cost
+
+if st.session_state['generated']:
+    with response_container:
+        for i in range(len(st.session_state['generated'])):
+            message(st.session_state["past"][i], is_user=True, key=str(i) + '_user')
+            message(st.session_state["generated"][i], key=str(i))
+            st.write(
+                f"Model used: {st.session_state['model_name'][i]}; Number of tokens: {st.session_state['total_tokens'][i]}; Cost: ${st.session_state['cost'][i]:.5f}")
+            counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
